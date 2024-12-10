e28cae36d200c55e4371c67746ca9fc942287b30
kurniawan <kurni.aawann19@gmail.com>
2024-12-10 22:26:54 +0700

    Setup Project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CommanModule } from './comman/comman.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommanModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
